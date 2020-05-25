f957e3b2204efb19e7304665d2ba8dc961113f4e
liquanwei <liquanwei@corp.netease.com>
2020-05-25 11:54:32 +0800

    feat: update test

--- test/index.test.js
+++ test/index.test.js
@@ -11,4 +11,33 @@ describe('shallow', () => {
         const app = shallow(<Test className="test" items={items} getKey={() => Math.random()} render={a => a} />);
         expect(app.find('.test').text()).toBe('');
     });
+
+    test('test scroll', (done) => {
+        const items = [{name: 'a'}, { name: 'b'}];
+
+        function makeEntries(toppx) {
+            return [{
+                boundingClientRect: { top: toppx },
+                rootBounds: { top: 0}
+            }]
+        }
+        global.window.IntersectionObserver = function (callback) {
+            setTimeout(() => {
+                expect(() => {
+                    callback(makeEntries(10));
+                    callback(makeEntries(100));
+                }).not.toThrow();
+
+                expect(() => {
+                    callback(makeEntries(100));
+                    callback(makeEntries(10));
+                }).not.toThrow();
+                done();
+            }, 100);
+
+            return { observe: () => {}, disconnect: () => {}};
+        };
+
+        mount(<Test className="test" items={items} getKey={() => Math.random()} render={a => a.name } />);
+    })
 });
