ab73bccc781cd73a2570a021c74cb5e47dbd8258
liquanwei <liquanwei@corp.netease.com>
2020-05-26 14:07:51 +0800

    feat: modify demo

--- demo/app.js
+++ demo/app.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import ReactDOM from 'react-dom';
 import SmartList from '../src/index';
+import FoldableSmartList from '../src/FoldableWindowVirtualScroll';
 
 const list1 = (() => {
     const ary = [];
@@ -29,6 +30,7 @@ function renderItem({ item }) {
 }
 const App = () => {
     const [list, setList] = useState(list1);
+    const [toggle, setToggle] = useState(true);
 
     const onClick = useCallback(() => {
         setList(i => (i === list1 ? list2 : list1));
@@ -37,7 +39,29 @@ const App = () => {
     return (
         <div>
             <div onClick={onClick}>switch list</div>
-            <SmartList items={list} render={renderItem} getKey={getKey} height={50} />
+            <div style={{position: 'relative', paddingTop: '30px'}}>
+                <div style={{ position: 'absolute', width: '50%', left: 0, top: 0}}>
+                    <div>WindowVirtualScroll</div>
+                    <SmartList items={list} render={renderItem} getKey={getKey} height={100}>
+                        <div>empty1</div>
+                    </SmartList>
+                    <div style={{height: '600px'}}>worldismine</div>
+                </div>
+                <div style={{ position: 'absolute', width: '50%', left: '50%', top: 0}}>
+                    <div>FoldableWindowVirtualScroll</div>
+                    <FoldableSmartList
+                        enableFold={true}
+                        foldStart={1}
+                        foldEnd={20}
+                        foldHeight={200}
+                        foldChildren={({ toggleFold }) => <div onClick={toggleFold}>fold children</div>}
+                        items={list}
+                        render={renderItem}
+                        getKey={getKey}
+                        height={100}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
